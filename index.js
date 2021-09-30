120ce2cbf9451620b11bf7cf44e24c45c59d1b7d
Salim Al-Ateeqi <76257198+CaptSoulBeard@users.noreply.github.com>
2021-10-01 00:56:54 +0300

    Challenge 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// Challenge 1
+
+// 1
+
+function sum() {
+  let numbers = [1, 2, 3, 4, 999];
+  let b = Number(numbers.slice(-1));
+  a = numbers[0];
+
+  return (sum = a + b);
+}
+console.log(sum());
+
+// 2
+
+function alwaysFive() {
+  var temp = "AMAAAAZING";
+  Z = (temp.match(/A/g) || []).length;
+  return Z;
+}
+console.log(alwaysFive());
